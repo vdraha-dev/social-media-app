baca18a1a9e50d1204bceb406a088d8e8a5ecf12
vdraha <vladyslav.draha.dev@gmail.com>
2026-06-08 18:14:34 +0300

    feat: add login route

--- app/identity/presentation/router.py
+++ app/identity/presentation/router.py
@@ -1,9 +1,18 @@
 from fastapi import APIRouter
 
-from app.identity.application.dto import RegisterUserRequest, UserResponse
+from app.identity.application.dto import (
+    LoginRequest,
+    RegisterUserRequest,
+    TokenResponse,
+    UserResponse,
+)
 
 auth = APIRouter(prefix="auth")
 
 
 @auth.post("/register", response_model=UserResponse, status_code=201)
 async def register(request: RegisterUserRequest): ...
+
+
+@auth.post("/login", response_model=TokenResponse, status_code=200)
+async def login(request: LoginRequest): ...
