4e55ac672b1f136f8404c965514eac13e94ac975
vdraha <vladyslav.draha.dev@gmail.com>
2026-06-10 14:29:26 +0300

    feat: add TokenAccessModel

--- app/identity/infrastructure/models.py
+++ app/identity/infrastructure/models.py
@@ -1,9 +1,10 @@
 from datetime import datetime
+from uuid import UUID
 
 from click import DateTime
+from sqlalchemy import Boolean, ForeignKey, String
 from sqlalchemy import Enum as SqlEnum
-from sqlalchemy import String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.identity.domain.value_objects import RoleEnum
 from app.shared.infrastructure.database import Base
@@ -20,3 +21,21 @@ class UserModel(Base):
         DateTime(timezone=True), nullable=True
     )
 
+    # relationships
+
+    tokens: Mapped[list[AccessToken]] = relationship(
+        back_populates="user", cascade="all, delete-orphan"
+    )
+
+
+class AccessTokenModel(Base):
+    __tablename__ = "access_tokens"
+
+    token: Mapped[str] = mapped_column(String(512), nullable=False, unique=True)
+    user_id: Mapped[UUID] = mapped_column(UUID(as_uuid=True), ForeignKey("users.id"))
+    expire_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=False)
+    blacklisted: Mapped[bool] = mapped_column(Boolean)
+
+    # relationships
+
+    user: Mapped[UserModel] = relationship(back_populates="tokens")
