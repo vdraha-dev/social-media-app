4d54d92b502ee676ce2d111b63230476b860c338
vdraha <vladyslav.draha.dev@gmail.com>
2026-06-11 23:24:36 +0300

    chore: ruff check

--- alembic/versions/bea8099fe81f_init.py
+++ alembic/versions/bea8099fe81f_init.py
@@ -1,21 +1,18 @@
 """init
 
 Revision ID: bea8099fe81f
-Revises: 
+Revises:
 Create Date: 2026-06-11 19:27:31.131516
 
 """
-from typing import Sequence, Union
 
-from alembic import op
-import sqlalchemy as sa
-
+from collections.abc import Sequence
 
 # revision identifiers, used by Alembic.
-revision: str = 'bea8099fe81f'
-down_revision: Union[str, Sequence[str], None] = None
-branch_labels: Union[str, Sequence[str], None] = None
-depends_on: Union[str, Sequence[str], None] = None
+revision: str = "bea8099fe81f"
+down_revision: str | Sequence[str] | None = None
+branch_labels: str | Sequence[str] | None = None
+depends_on: str | Sequence[str] | None = None
 
 
 def upgrade() -> None:
