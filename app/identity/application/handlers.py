9b5dc566030678261927430b4e88e20c2794d1e9
vdraha <vladyslav.draha.dev@gmail.com>
2026-06-11 18:46:44 +0300

    feat: add LoginHandler

--- app/identity/application/handlers.py
+++ app/identity/application/handlers.py
@@ -1,10 +1,20 @@
-from app.identity.application.dto import RegisterUserRequest, UserResponse
+from app.identity.application.dto import (
+    LoginRequest,
+    RegisterUserRequest,
+    TokenResponse,
+    UserResponse,
+)
 from app.identity.domain.entities import User
-from app.identity.domain.errors import UserAlreadyExistsError
-from app.identity.domain.events import UserRegistered
-from app.identity.domain.service import IPasswordHasher, IUserRepository
+from app.identity.domain.errors import InvalidCredentialsError, UserAlreadyExistsError
+from app.identity.domain.events import UserLogedIn, UserRegistered
+from app.identity.domain.service import (
+    IAccessTokenRepository,
+    IPasswordHasher,
+    ITokenService,
+    IUserRepository,
+)
 from app.identity.domain.value_objects import Role
-from app.shared.events import event_bus
+from app.shared.events.event_bus import event_bus
 
 
 class RegisterUserhandler:
@@ -30,4 +40,38 @@ class RegisterUserhandler:
 
         return UserResponse(
             user_id=user.id, username=user.username, email=user.email, role=user.role
+        )
+
+
+class LoginHandler:
+    def __init__(
+        self,
+        user_repo: IUserRepository,
+        token_repo: IAccessTokenRepository,
+        hasher: IPasswordHasher,
+        token_service: ITokenService,
+    ):
+        self.user_repo = user_repo
+        self.token_repo = token_repo
+        self.hasher = hasher
+        self.token_service = token_service
+
+    async def handle(self, request: LoginRequest):
+        user = await self.user_repo.get_by_email(request.email)
+
+        if not user or self.hasher.verify(user.password, request.password):
+            raise InvalidCredentialsError(
+                "Invalid credentials. Wrong email or password."
+            )
+
+        user.record_login()
+        await self.user_repo.save(user)
+
+        token = self.token_service.generate(user.id, user.role)
+        await self.token_repo.save(token)
+
+        await event_bus.publish(UserLogedIn(user_id=user.id))
+
+        return TokenResponse(
+            access_token=token.access_token, token_type="Bearer"
         )
