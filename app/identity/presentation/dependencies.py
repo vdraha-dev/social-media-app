f196a00ff96f5b5e5f4fe1aadd6602288321c534
vdraha <vladyslav.draha.dev@gmail.com>
2026-06-12 15:25:25 +0300

    feat: add dependencies

--- app/identity/presentation/dependencies.py
+++ app/identity/presentation/dependencies.py
@@ -1,4 +1,12 @@
+from fastapi import Depends
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+
 from app.identity.domain.entities import User
 
+bearer_scheme = HTTPBearer()
 
-async def get_current_user() -> User: ...
+
+async def get_current_user_token(
+    credentials: HTTPAuthorizationCredentials = Depends(bearer_scheme),
+) -> User:
+    return credentials.credentials
