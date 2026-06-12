7b27b750df5df308af1073c54767ed49e66ed690
vdraha <vladyslav.draha.dev@gmail.com>
2026-06-12 14:01:20 +0300

    feat: connect exception handlers

--- app/main.py
+++ app/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
 
+from app.identity.presentation.exception_handlers import (
+    register_exception_handlers as identity_handlers,
+)
 from app.identity.presentation.router import auth
+from app.shared.presentation.exception_handlers import (
+    register_exception_handlers as shared_handlers,
+)
 
 app = FastAPI(docs_url="/docs", title="Social Media App")
+shared_handlers(app)
 
 app.include_router(auth)
+identity_handlers(app)
