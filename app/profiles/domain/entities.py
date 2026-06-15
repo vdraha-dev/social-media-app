3bddfd65b5f7fe98f30f9e9184c65abe00f131e3
vdraha <vladyslav.draha.dev@gmail.com>
2026-06-15 19:38:41 +0300

    feat: add UserProfile entity

---
+++ app/profiles/domain/entities.py
@@ -0,0 +1,34 @@
+from dataclasses import dataclass, field
+from uuid import UUID
+
+from app.profiles.domain.value_objects import Bio, DisplayedName, SocialLink
+from app.shared.domain.base_entity import BaseEntity
+from app.shared.domain.value_objects import Url
+
+
+@dataclass(slots=True)
+class UserProfile(BaseEntity):
+    user_id: UUID
+    displayed_name: DisplayedName
+    avatart_url: Url | None = None
+    bio: Bio | None = None
+    social_links: list[SocialLink] = field(default_factory=list)
+
+    def set_displayed_name(self, new_displayed_name: DisplayedName):
+        self.displayed_name = new_displayed_name
+        self._touch()
+
+    def set_avatar_url(self, new_avatar_url: Url):
+        self.avatart_url = new_avatar_url
+        self._touch()
+
+    def set_bio(self, new_bio: Bio):
+        self.bio = new_bio
+        self._touch()
+
+    def add_social_link(self, social_link: SocialLink):
+        if social_link in self.social_links:
+            return
+
+        self.social_links.append(social_link)
+        self._touch()
