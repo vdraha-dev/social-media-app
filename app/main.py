1ca7615f0c634d82e1820977426faffe5d5c6054
vdraha <vladyslav.draha.dev@gmail.com>
2026-06-17 17:40:25 +0300

    feat: connect profiles routes

--- app/main.py
+++ app/main.py
@@ -4,6 +4,10 @@ from app.identity.presentation.exception_handlers import (
     register_exception_handlers as identity_handlers,
 )
 from app.identity.presentation.router import auth
+from app.profiles.presentation.exception_handlers import (
+    register_exception_handlers as profiles_handlers,
+)
+from app.profiles.presentation.routers import profiles
 from app.shared.presentation.exception_handlers import (
     register_exception_handlers as shared_handlers,
 )
@@ -13,3 +17,6 @@ shared_handlers(app)
 
 app.include_router(auth)
 identity_handlers(app)
+
+app.include_router(profiles)
+profiles_handlers(app)
