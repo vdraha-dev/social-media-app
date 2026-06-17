e0a792cfec26ff7a31da27df0ef32f09348bed8a
vdraha <vladyslav.draha.dev@gmail.com>
2026-06-17 10:55:28 +0300

    feat: change userProfile without using dataclass

--- app/profiles/domain/entities.py
+++ app/profiles/domain/entities.py
@@ -1,4 +1,5 @@
-from dataclasses import dataclass, field
+from collections.abc import Iterable
+from datetime import datetime
 from uuid import UUID
 
 from app.profiles.domain.value_objects import Bio, DisplayedName, SocialLink
@@ -8,11 +9,51 @@ from app.shared.domain.value_objects import Url
 
 @dataclass(slots=True)
 class UserProfile(BaseEntity):
-    user_id: UUID
-    displayed_name: DisplayedName
-    avatar_url: Url | None = None
-    bio: Bio | None = None
-    social_links: list[SocialLink] = field(default_factory=list[SocialLink])
+    __slots__ = (
+        "_user_id",
+        "_displayed_name",
+        "_avatar_url",
+        "_bio",
+        "_social_links",
+    )
+
+    def __init__(
+        self,
+        user_id: UUID,
+        displayed_name: DisplayedName,
+        avatar_url: Url | None = None,
+        bio: Bio | None = None,
+        social_links: list[SocialLink] | None = None,
+        id: UUID | None = None,
+        created_at: datetime | None = None,
+        updated_at: datetime | None = None,
+    ):
+        super().__init__(id, created_at, updated_at)
+        self._user_id = user_id
+        self._displayed_name = displayed_name
+        self._avatar_url = avatar_url
+        self._bio = bio
+        self._social_links = social_links if social_links else list[SocialLink]()
+
+    @property
+    def user_id(self) -> UUID:
+        return self._user_id
+
+    @property
+    def displayed_name(self) -> DisplayedName:
+        return self._displayed_name
+
+    @property
+    def avatar_url(self) -> Url | None:
+        return self._avatar_url
+
+    @property
+    def bio(self) -> Bio | None:
+        return self._bio
+
+    @property
+    def social_links(self) -> tuple[SocialLink, ...]:
+        return tuple(self._social_links)
 
     def set_displayed_name(self, new_displayed_name: DisplayedName):
         self.displayed_name = new_displayed_name
