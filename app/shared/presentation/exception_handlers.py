cf356d86997a3cfcf512d733fcaac8d280fe866a
vdraha <vladyslav.draha.dev@gmail.com>
2026-06-17 19:29:00 +0300

    style: ignore pyright errors

--- app/shared/presentation/exception_handlers.py
+++ app/shared/presentation/exception_handlers.py
@@ -13,19 +13,19 @@ from app.shared.domain.exceptions import (
 
 def register_exception_handlers(app: FastAPI):
     @app.exception_handler(NotFoundError)
-    async def not_found_error_handler(request: Request, exc: NotFoundError):
+    async def not_found_error_handler(request: Request, exc: NotFoundError):  # pyright: ignore
         return JSONResponse(
             status_code=404, content={"detail": str(exc), "code": "NOT_FOUND"}
         )
 
     @app.exception_handler(AlreadyExistsError)
-    async def already_exists_error_handler(request: Request, exc: AlreadyExistsError):
+    async def already_exists_error_handler(request: Request, exc: AlreadyExistsError):  # pyright: ignore
         return JSONResponse(
             status_code=409, content={"detail": str(exc), "code": "ALREADY_EXISTS"}
         )
 
     @app.exception_handler(PermissionDeniedError)
-    async def permission_denied_error_handler(
+    async def permission_denied_error_handler(  # pyright: ignore
         request: Request, exc: PermissionDeniedError
     ):
         return JSONResponse(
@@ -33,19 +33,19 @@ def register_exception_handlers(app: FastAPI):
         )
 
     @app.exception_handler(ValidationError)
-    async def validation_error_handler(request: Request, exc: ValidationError):
+    async def validation_error_handler(request: Request, exc: ValidationError):  # pyright: ignore
         return JSONResponse(
             status_code=422, content={"detail": str(exc), "code": "VALIDATION_ERROR"}
         )
 
     @app.exception_handler(ConflictError)
-    async def conflict_error_handler(request: Request, exc: ConflictError):
+    async def conflict_error_handler(request: Request, exc: ConflictError):  # pyright: ignore
         return JSONResponse(
             status_code=409, content={"detail": str(exc), "code": "CONFLICT"}
         )
 
     @app.exception_handler(DomainError)
-    async def domain_error_handler(request: Request, exc: DomainError):
+    async def domain_error_handler(request: Request, exc: DomainError):  # pyright: ignore
         return JSONResponse(
             status_code=400, content={"detail": str(exc), "code": "DOMAIN_ERROR"}
         )
