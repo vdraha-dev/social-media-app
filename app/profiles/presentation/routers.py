c6223abcb2c0383eb93530b8feb7b2ec497d2e04
vdraha <vladyslav.draha.dev@gmail.com>
2026-06-17 19:02:24 +0300

    feat: add updte profile handle

--- app/profiles/presentation/routers.py
+++ app/profiles/presentation/routers.py
@@ -10,9 +10,14 @@ from app.identity.presentation.router import (
     TokenService,
     UserRepository,
 )
-from app.profiles.application.dto import ProfileResponse
-from app.profiles.application.use_cases import GetUserProfileByUserIdUseCase
+from app.profiles.application.dto import ProfileResponse, UpdateProfileRequest
+from app.profiles.application.use_cases import (
+    GetUserProfileByUserIdUseCase,
+    UpdateUserProfileUseCase,
+)
+from app.profiles.domain.value_objects import Bio, DisplayedName
 from app.profiles.infrastructure.repositories import ProfilesRepository
+from app.shared.domain.value_objects import Url
 from app.shared.infrastructure.database import UnitOfWork, get_uow
 
 profiles = APIRouter(prefix="/profiles", tags=["profiles"])
@@ -28,6 +33,10 @@ async def auth_user_dependency(uow: UnitOfWork = Depends(get_uow)):
 
 async def get_profile_by_user_id(uow: UnitOfWork = Depends(get_uow)):
     return GetUserProfileByUserIdUseCase(ProfilesRepository(uow.session))
+
+
+async def update_profile_dependency(uow: UnitOfWork = Depends(get_uow)):
+    return UpdateUserProfileUseCase(ProfilesRepository(uow.session))
 
 
 @profiles.get("/{user_id}", response_model=ProfileResponse)
@@ -62,5 +71,39 @@ async def get_me(
     )
 
 
-@profiles.put("/me")
-async def update_profile(): ...
+@profiles.put("/me", response_model=ProfileResponse)
+async def update_profile(
+    request: UpdateProfileRequest,
+    user_token: str = Depends(get_current_user_token),
+    auth_use_case: AuthenticateUserByTokenUseCase = Depends(auth_user_dependency),
+    get_profile_use_case: GetUserProfileByUserIdUseCase = Depends(
+        get_profile_by_user_id
+    ),
+    update_profile_use_case: UpdateUserProfileUseCase = Depends(
+        update_profile_dependency
+    ),
+):
+    user = await auth_use_case.execute(user_token)
+    profile = await get_profile_use_case.execute(user.id)
+
+    updated_at = profile.updated_at
+
+    profile.set_displayed_name(
+        DisplayedName(request.display_name)
+        if request.display_name
+        else profile.displayed_name
+    )
+    profile.set_avatar_url(
+        Url(request.avatar_url) if request.avatar_url else profile.avatar_url
+    )
+    profile.set_bio(Bio(request.bio) if request.bio else profile.bio)
+
+    if updated_at != profile.updated_at:
+        await update_profile_use_case.execute(profile)
+
+    return ProfileResponse(
+        user_id=profile.user_id,
+        display_name=str(profile.displayed_name),
+        bio=str(profile.bio),
+        social_links=[link.to_dict() for link in profile.social_links],
+    )
