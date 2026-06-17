d2bb7db2e67d617fd39e93e52692e94b7d73cc38
vdraha <vladyslav.draha.dev@gmail.com>
2026-06-17 21:30:06 +0300

    fix: ignore pyright error

--- app/identity/presentation/exception_handlers.py
+++ app/identity/presentation/exception_handlers.py
@@ -11,17 +11,17 @@ from app.identity.domain.exceptions import (
 
 def register_exception_handlers(app: FastAPI):
     @app.exception_handler(TokenExpiredError)
-    async def token_expired_error(request: Request, err: TokenExpiredError):
+    async def token_expired_error(request: Request, err: TokenExpiredError):  # pyright: ignore
         return JSONResponse(content=str(err), status_code=403)
 
     @app.exception_handler(InvalidTokenError)
-    async def invalid_token_error(request: Request, err: InvalidTokenError):
+    async def invalid_token_error(request: Request, err: InvalidTokenError):  # pyright: ignore
         return JSONResponse(content=str(err), status_code=403)
 
     @app.exception_handler(UserAlreadyExistsError)
-    async def user_already_exist_error(request: Request, err: UserAlreadyExistsError):
+    async def user_already_exist_error(request: Request, err: UserAlreadyExistsError):  # pyright: ignore
         return JSONResponse(content=str(err), status_code=409)
 
     @app.exception_handler(InvalidCredentialsError)
-    async def invalid_credentials_error(request: Request, err: InvalidCredentialsError):
+    async def invalid_credentials_error(request: Request, err: InvalidCredentialsError):  # pyright: ignore
         return JSONResponse(content=str(err), status_code=401)
