33b564f52c89aedbb71a40bd309628d10c5fcbad
vdraha <vladyslav.draha.dev@gmail.com>
2026-06-21 18:47:23 +0300

    refactor: all code flow moved into use cases

--- app/identity/presentation/router.py
+++ app/identity/presentation/router.py
@@ -1,23 +1,21 @@
 from fastapi import APIRouter, Depends
 
-from app.identity.application.use_cases import (
-    AuthenticateUserByTokenUseCase,
-    LoginUseCase,
-    LogoutUseCase,
-    RegisterUserUseCase,
-)
-from app.identity.domain.entities import User
-from app.identity.domain.value_objects import HashedPassword, Role, RoleEnum, UserName
-from app.identity.infrastructure.repository import AccessTokenRepository, UserRepository
-from app.identity.infrastructure.security import PasswordHasher, TokenService
-from app.identity.presentation.dependencies import get_current_user_token
-from app.identity.presentation.dto import (
+from app.identity.application.dto import (
     LoginRequest,
     RegisterUserRequest,
     TokenResponse,
     UserResponse,
 )
-from app.shared.domain.value_objects import Email
+from app.identity.application.use_cases import (
+    AuthenticateUserByTokenUseCase,
+    GetUserInfoByTokenUseCase,
+    LoginUseCase,
+    LogoutUseCase,
+    RegisterUserUseCase,
+)
+from app.identity.infrastructure.repository import AccessTokenRepository, UserRepository
+from app.identity.infrastructure.security import PasswordHasher, TokenService
+from app.identity.presentation.dependencies import get_current_user_token
 from app.shared.infrastructure.database import UnitOfWork, get_uow
 
 auth = APIRouter(prefix="/auth", tags=["authentication"])
@@ -46,9 +44,16 @@ async def auth_user_dependency(
     uow: UnitOfWork = Depends(get_uow),
 ) -> AuthenticateUserByTokenUseCase:
     return AuthenticateUserByTokenUseCase(
-        UserRepository(uow.session),
         AccessTokenRepository(uow.session),
         TokenService(),
+    )
+
+
+async def user_info_dependency(
+    uow: UnitOfWork = Depends(get_uow),
+) -> GetUserInfoByTokenUseCase:
+    return GetUserInfoByTokenUseCase(
+        UserRepository(uow.session),
     )
 
 
@@ -57,32 +62,18 @@ async def register(
     request: RegisterUserRequest,
     use_case: RegisterUserUseCase = Depends(register_dependency),
 ):
-    user = User(
-        username=UserName(request.username),
-        email=Email(request.email),
-        password=HashedPassword(value="0"),
-        role=Role(RoleEnum.User),
-    )
-    await use_case.execute(user, request.password)
+    response = await use_case.execute(request)
 
-    return UserResponse(
-        id=user.id,
-        username=str(user.username),
-        email=str(user.email),
-        role=str(user.role),
-    )
+    return response
 
 
 @auth.post("/login", response_model=TokenResponse, status_code=200)
 async def login(
     request: LoginRequest, use_case: LoginUseCase = Depends(login_dependency)
 ):
-    token = await use_case.execute(Email(request.email), request.password)
+    response = await use_case.execute(request)
 
-    return TokenResponse(
-        access_token=token,
-        token_type="Bearer",
-    )
+    return response
 
 
 @auth.post("/logout", status_code=204)
@@ -91,19 +82,16 @@ async def logout(
     auth_use_case: AuthenticateUserByTokenUseCase = Depends(auth_user_dependency),
     logout_use_case: LogoutUseCase = Depends(logout_dependency),
 ):
-    user = await auth_use_case.execute(current_user_token)
-    await logout_use_case.execute(user.id)
+    user_id = await auth_use_case.execute(current_user_token)
+    await logout_use_case.execute(user_id)
 
 
 @auth.get("/me", response_model=UserResponse, status_code=200)
 async def get_me(
     current_user_token: str = Depends(get_current_user_token),
     auth_use_case: AuthenticateUserByTokenUseCase = Depends(auth_user_dependency),
+    user_use_case: GetUserInfoByTokenUseCase = Depends(user_info_dependency),
 ):
-    user = await auth_use_case.execute(current_user_token)
-    return UserResponse(
-        id=user.id,
-        username=str(user.username),
-        email=str(user.email),
-        role=str(user.role),
-    )
+    user_id = await auth_use_case.execute(current_user_token)
+    response = await user_use_case.execute(user_id)
+    return response
